6970912b7275c224c3bc31fce4b3d3fb8ee5864e
aymen.bouhaha <aymen.bouhaha@6two8.com>
2025-01-22 19:19:15 +0100

    Implements project configuration

---
+++ tailwind.config.js
@@ -0,0 +1,70 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	darkMode: ["class"],
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		fontFamily: {
+			sans: ["Inter", "sans-serif"],
+		},
+		fontSize: {
+			h1: [
+				"18px",
+				{
+					fontWeight: "500",
+				},
+			],
+			p1: [
+				"16px",
+				{
+					fontWeight: "500",
+				},
+			],
+			p2: [
+				"14px",
+				{
+					fontWeight: "500",
+				},
+			],
+			p21: [
+				"14px",
+				{
+					fontWeight: "400",
+				},
+			],
+			p3: [
+				"12px",
+				{
+					fontWeight: "400",
+				},
+			],
+			p4: [
+				"10px",
+				{
+					fontWeight: "400",
+				},
+			],
+		},
+		screens: {
+			"2xl": { max: "1535px" },
+			xl: { max: "1279px" },
+			lg: { max: "1023px" },
+			md: { max: "767px" },
+			sm: { max: "639px" },
+		},
+		colors: {
+			background: "#FCFCFC",
+			"primary-black": "#242731",
+			"secondary-black": "#151A1F",
+			"primary-white": "#E0E4E7",
+			"primary-purple": "#A162F7",
+			"primary-orange": "#FF764C",
+			"primary-green": "#70CF97",
+			"primary-red": "#FF0015",
+			"grey-1": "#A4A5A6",
+			"grey-2": "#7C7C8D",
+			"grey-3": "#72767C",
+			"grey-border": "#E9EAEB",
+		},
+	},
+	plugins: [require("tailwindcss-animate")],
+};
