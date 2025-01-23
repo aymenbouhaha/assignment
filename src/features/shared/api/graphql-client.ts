ef06dfc6a6acabd06ca0f3eba090bb3b0992e10f
aymen.bouhaha <aymen.bouhaha@6two8.com>
2025-01-24 00:47:35 +0100

    Add env var

--- src/features/shared/api/graphql-client.ts
+++ src/features/shared/api/graphql-client.ts
@@ -5,6 +5,6 @@ export const client = new ApolloClient({
 	uri: "https://api.github.com/graphql",
 	cache: new InMemoryCache(),
 	headers: {
-		Authorization: `Bearer `,
+		Authorization: `Bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}`,
 	},
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import viteTsconfigPaths from "vite-tsconfig-paths";
@@ -8,25 +8,31 @@ import mkcert from "vite-plugin-mkcert";
 import checker from "vite-plugin-checker";
 
 // https://vite.dev/config/
-export default defineConfig({
-	base: "/",
-	plugins: [
-		react(),
-		viteTsconfigPaths(),
-		svgr(),
-		eslint(),
-		mkcert(),
-		checker({
-			typescript: true,
-		}),
-	],
-	server: {
-		open: true,
-		port: 3000,
-	},
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), "");
+	return {
+		define: {
+			"process.env": env,
 		},
-	},
+		base: "/",
+		plugins: [
+			react(),
+			viteTsconfigPaths(),
+			svgr(),
+			eslint(),
+			mkcert(),
+			checker({
+				typescript: true,
+			}),
+		],
+		server: {
+			open: true,
+			port: 3000,
+		},
+		resolve: {
+			alias: {
+				"@": path.resolve(__dirname, "./src"),
+			},
+		},
+	};
 });
