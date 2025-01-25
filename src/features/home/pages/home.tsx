ad34e9cd7eeb77a053542d1cd8730754d8e3612c
aymen.bouhaha <aymen.bouhaha@6two8.com>
2025-01-25 02:28:26 +0100

    Implements navigation between pages

--- src/features/home/pages/home.tsx
+++ src/features/home/pages/home.tsx
@@ -3,12 +3,17 @@ import { useForm } from "react-hook-form";
 import { GithubLoginForm, GithubLoginFormSchema } from "@home/models/github-login-form.ts";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { WelcomeCard } from "@home/components/welcome-card.tsx";
+import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
 	const form = useForm<GithubLoginForm>({
 		resolver: zodResolver(GithubLoginFormSchema),
 	});
-	const onSubmit = () => {};
+	const navigate = useNavigate();
+
+	const onSubmit = (data: GithubLoginForm) => {
+		navigate(`/repositories/${data.login}`);
+	};
 
 	return (
 		<div className="flex h-screen w-full items-center justify-center">
